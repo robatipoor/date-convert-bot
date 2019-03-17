6db12da8e00fbe06cd77e2e7e90e324007bd4d04
robatioor <mahdi.robatipoor@gmail.com>
2019-03-17 08:11:06 +0330

    add eslint

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+/* eslint-disable no-console */
+
 const jalaali = require('jalaali-js')
 const Telegraf = require('telegraf')
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+
 bot.start((ctx) => {
     console.log(ctx.options.username + ' Bot Start !');
     ctx.reply('Welcome !');
 })
+
 bot.on('text', async (ctx) => {
     const text = ctx.update.message.text;
     parseDate(text).then((date) => {
         console.log(date);
         return validDate(date);
     }, (err) => {
+        console.log(err);
     }).then((date) => {
         console.log(date);
         return outputDate(date);
@@ -23,14 +28,13 @@ bot.on('text', async (ctx) => {
         console.log(err);
     });
 })
+
 bot.launch()
 
 function outputDate(date) {
-    return new Promise((resolve, reject) => {
-        const gdate = jalaali.toGregorian(date['year'], date['month'], date['day'])
-        let result = gdate['gy'] + '-' + gdate['gm'] + '-' + gdate['gd'];
-        resolve(result);
-    });
+    const gdate = jalaali.toGregorian(date['year'], date['month'], date['day'])
+    let result = gdate['gy'] + '-' + gdate['gm'] + '-' + gdate['gd'];
+    return new Promise.resolve(result);
 }
 
 function validDate(date) {
